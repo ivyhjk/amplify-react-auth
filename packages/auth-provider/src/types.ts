03c5172501f7aa8198af0ec22afc5390053624de
ivyhjk <elvis.munoz.f@gmail.com>
2020-12-27 16:56:21 -0300

    AuthContextValue now extends to AuthCoreContextValue

--- packages/auth-provider/src/types.ts
+++ packages/auth-provider/src/types.ts
@@ -1,6 +1,6 @@
-import { AuthState } from '@ivyhjk/amplify-react-auth-provider-core';
+import { AuthCoreContextValue } from '@ivyhjk/amplify-react-auth-provider-core';
 
-export interface AuthContextValue extends AuthState {
+export interface AuthContextValue extends Omit<AuthCoreContextValue, 'dispatch'> {
   signIn: (username: string, password: string) => void;
   signOut: () => void;
 }
