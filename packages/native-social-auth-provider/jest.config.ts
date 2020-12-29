35e03275ac6eca5699e8a32ca71088553f466d79
ivyhjk <elvis.munoz.f@gmail.com>
2020-12-29 16:35:24 -0300

    setup react native jest environment

--- packages/native-social-auth-provider/jest.config.ts
+++ packages/native-social-auth-provider/jest.config.ts
@@ -3,8 +3,15 @@ import type { Config } from '@jest/types';
 import baseConfig from '../../jest.config.base';
 
 const config: Config.InitialOptions = {
-  ...baseConfig
-  // testEnvironment: 'node'
+  ...baseConfig,
+  setupFiles: [
+    '<rootDir>/src/__tests__/jest.setup.js'
+  ],
+  preset: 'react-native',
+  transform: {
+    '^.+\\.(js)$': '<rootDir>/../../node_modules/react-native/jest/preprocessor.js',
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
 };
 
 export default config;
