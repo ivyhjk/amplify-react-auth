b5e605656dbab8c155beaacda6f759151dc2ce67
ivyhjk <elvis.munoz.f@gmail.com>
2020-12-29 16:27:31 -0300

    add jest setup files, for react native libraries

---
+++ packages/native-social-auth-provider/src/__tests__/jest.setup.js
@@ -0,0 +1,14 @@
+import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
+
+jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
+jest.mock('@react-native-community/google-signin', () => ({
+  GoogleSignin: {
+    hasPlayServices: jest.fn(),
+    signIn: jest.fn(),
+    signOut: jest.fn()
+  },
+  statusCodes: {
+    SIGN_IN_CANCELLED: 'SIGN_IN_CANCELLED',
+    SIGN_IN_REQUIRED: 'SIGN_IN_REQUIRED'
+  }
+}));
