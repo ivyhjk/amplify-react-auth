57d6efde64f4633da0639e4677ab1d6a4ff76ff0
ivyhjk <elvis.munoz.f@gmail.com>
2020-12-29 16:14:20 -0300

    add main exporter

---
+++ packages/native-social-auth-provider/src/utils/index.ts
@@ -0,0 +1,3 @@
+export { default as federatedSignIn } from './federatedSignIn';
+export { default as googleSignIn } from './googleSignIn';
+export { default as googleSignOut } from './googleSignOut';
