07aaf49d80f673adc2206125adba701be8a3f00a
ivyhjk <elvis.munoz.f@gmail.com>
2021-01-06 21:41:34 -0300

    add main exporter

---
+++ packages/oauth/src/index.ts
@@ -0,0 +1,7 @@
+export * from '@ivyhjk/amplify-react-core-auth';
+
+export { default as OAuthProvider } from './OAuthProvider';
+
+export * from './hooks';
+export * from './OAuthContext';
+export * from './types';
