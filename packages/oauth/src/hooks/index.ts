5a98b25d5ef01c4832ccd43e4d52c3dfecbb2cb8
ivyhjk <elvis.munoz.f@gmail.com>
2021-03-10 15:42:23 -0300

    use own types for core hooks

--- packages/oauth/src/hooks/index.ts
+++ packages/oauth/src/hooks/index.ts
@@ -1,3 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import {
+  useCurrentAuthenticatedUser,
+  useUser
+} from '@ivyhjk/amplify-react-core-auth';
+
+import { OAuthUser } from '../types';
+
 export { default as useFederatedSignIn } from './useFederatedSignIn';
 export { default as useSignOut } from './useSignOut';
 export { default as useUserPayload } from './useUserPayload';
+
+export const useCurrentOAuthAuthenticatedUser = () => useCurrentAuthenticatedUser<OAuthUser>();
+export const useOAuthUser = () => useUser<OAuthUser>();
