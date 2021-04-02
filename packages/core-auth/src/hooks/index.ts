d1072003afb0daa983ce2e932c8927ba27e2a0c1
ivyhjk <elvis.munoz.f@gmail.com>
2021-04-02 19:29:10 -0300

    export useAuthentication hook

--- packages/core-auth/src/hooks/index.ts
+++ packages/core-auth/src/hooks/index.ts
@@ -1,2 +1,3 @@
+export { default as useAuthentication } from './useAuthentication';
 export { default as useCurrentAuthenticatedUser } from './useCurrentAuthenticatedUser';
 export { default as useUser } from './useUser';
