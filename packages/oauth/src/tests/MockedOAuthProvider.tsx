3cb112311178cb97946a4154f5b845b04151babc
ivyhjk <elvis.munoz.f@gmail.com>
2021-04-05 20:29:43 -0400

    remove useless properties

--- packages/oauth/src/tests/MockedOAuthProvider.tsx
+++ packages/oauth/src/tests/MockedOAuthProvider.tsx
@@ -5,13 +5,9 @@ import React from 'react';
 
 import { OAuthContextProvider } from '../OAuthProvider';
 
-type MockedOAuthProviderProps = React.PropsWithChildren<{
-  loading: false
-}>;
-
 export default function MockedOAuthProvider<TUser> (
   { children, ...rest }: MockedCoreAuthProviderProps<TUser>
-): React.ReactElement<MockedOAuthProviderProps> {
+): React.ReactElement<MockedCoreAuthProviderProps<TUser>> {
   return (
     <MockedCoreAuthProvider {...rest}>
       <OAuthContextProvider>
